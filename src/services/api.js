2a182106f7f344ad6144a116a6fa75aa3c8d81bf
Sindhu-Attili <sindhuattili14@gmail.com>
2026-07-21 14:55:30 +0530

    Update API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "https://student-course-management-api-13d8.onrender.com/api/",
 });
-
-// Automatically attach JWT token to every request.
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default api;
